cc40a8738b640b6e3d31c3bfbf9349df81cdb7f4
王锐 <wangrui@hopos-MacBook-Pro.local>
2020-04-04 21:44:38 +0800

    update

--- write_custom_dataset_to_txt.py
+++ write_custom_dataset_to_txt.py
@@ -8,17 +8,25 @@ custom_images_path = os.path.join(dataset_path, "custom_images")
 if not os.path.exists(custom_images_path):
     os.mkdir(custom_images_path)
 
-#shutil.copy(dataset_label_txt_path, new_label_txt_path)
+shutil.copy(dataset_label_txt_path, new_label_txt_path)
 
+custom_image_files = os.listdir(custom_images_path)
 
-for file in os.listdir(custom_images_path):
-    label = os.path.splitext(file)[0].split("_")[1]
-    with open(new_label_txt_path, "a") as output_file:
-        output_file.writelines("{} {}\n".format(file, label))
+custom_number = len(custom_image_files)
+
+def add(image_files):
+    for file in image_files:
+        label = os.path.splitext(file)[0].split("_")[1]
+        with open(new_label_txt_path, "a") as output_file:
+            output_file.writelines("{} {}\n".format(file, label))
     #label = str(file).split(".")[0].split("_")[1]
     #if len(label) >1:
     #    print(label)
 
+times = 3600000//(custom_number * 100)
+
+for i in range(times):
+    add(custom_image_files)
 
 
 
